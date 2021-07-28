bb8677cd7c7728655fd4acf98e7a15a059159dab
afeiship <1290657123@qq.com>
2021-07-28 11:47:43 +0800

    feat: refactor

--- __tests__/index.spec.js
+++ __tests__/index.spec.js
@@ -2,28 +2,24 @@
   require('../src');
   require('@jswork/next-fetch-with-timeout');
   const fetch = require('node-fetch');
-  const newFetch = nx.fetchWithRetry(nx.fetchWithTimeout(fetch));
+  const betterFetch = nx.fetchWithRetry(fetch);
 
   jest.setTimeout(50 * 1000);
 
   describe('api.basic test', () => {
     test('nx.fetchWithRetry', function (done) {
-      newFetch('https://www.google.com', {
-        timeout: 1e3,
-        retryOn: function (attempt, error, response) {
-          console.log('error:', error);
-          // retry on any network error, or 4xx or 5xx status codes
-          if (error !== null || response.status >= 400) {
-            console.log(`retrying, attempt number ${attempt + 1}`);
-            return true;
-          }
-          return false;
-        }
-      })
-        .finally(() => {
+      betterFetch('https://www.google1.com/')
+        .then((res) => {
+          console.log('res');
+        })
+        .catch((err) => {
+          console.log('err::::', err);
+        })
+        .finally((e) => {
           console.log('done');
           done();
         });
+
       // const obj1 = { name: 'fei' };
       // const obj2 = { email: '1290657123@qq.com' };
       // const result = {};
--- src/index.js
+++ src/index.js
@@ -2,13 +2,36 @@
   var global = typeof window !== 'undefined' ? window : this || Function('return this')();
   var nx = global.nx || require('@jswork/next');
   var defaults = { retries: 3, retryDelay: 1000 };
-  var fetchRetry = require('fetch-retry');
+
+  // https://stackoverflow.com/questions/46175660/fetch-retry-request-on-failure
+
+  function wait(inDelay) {
+    return new Promise((resolve) => setTimeout(resolve, inDelay));
+  }
 
   nx.fetchWithRetry = function (inFetch) {
-    var enhancedFetch = fetchRetry(inFetch);
     return function (inUrl, inOptions) {
       var options = nx.mix(null, defaults, inOptions);
-      return enhancedFetch(inUrl, options);
+      var retriesLeft = options.retries;
+      var execute = function (left) {
+        return new Promise(function (resolve, reject) {
+          inFetch(inUrl, options)
+            .catch((error) => {
+              console.log('catch error?');
+              left = left - 1;
+              console.log('left:', left);
+              if (!left) {
+                reject(error);
+              } else {
+                console.log('retry logic?');
+                return wait(options.retryDelay).then(() => execute(left));
+              }
+            })
+            .then(resolve);
+        });
+      };
+
+      return execute(retriesLeft);
     };
   };
 
