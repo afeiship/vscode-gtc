9e86b036adebef4ac4f6c674225a23f92b455b12
afeiship <1290657123@qq.com>
2023-03-10 18:00:06 +0800

    fix: gtc help

--- src/extension.ts
+++ src/extension.ts
@@ -1,12 +1,11 @@
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
 export function activate(context: vscode.ExtensionContext) {
-    // 注册命令
-    context.subscriptions.push(
-        vscode.commands.registerCommand('gtc:help', () => {
-            // vscode show alert message
-            // execute command in terminal
-            vscode.window.showInformationMessage('Hello World!\n Are you ok?');
-        })
-    );
+  // 注册命令
+  // gtc:help
+  context.subscriptions.push(
+    vscode.commands.registerCommand("gtc:help", () => {
+      vscode.window.showInformationMessage("Gtc help?");
+    })
+  );
 }
